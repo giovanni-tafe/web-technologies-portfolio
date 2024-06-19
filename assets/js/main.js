3b0efdd13af7ed3499a8020dcfc9a995395e8d37
Giovanni TAFE <20114918@tafe.wa.edu.au>
2024-06-19 22:10:59 +0800

    fix: js reference

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,13 @@
 const menuToggle = document.getElementById("menuToggle");
-const navMenu = document.getElementById("nav");
 let mainMenu = document.getElementById('mainMenu')
 
 menuToggle.addEventListener('click', () => {
     mainMenu.classList.toggle('hidden')
     mainMenu.classList.toggle('flex')
     mainMenu.classList.toggle('gap-4')
+    mainMenu.classList.toggle("open");
 });
 
-// Listeners
-menuToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("open");
-});
 
 
 
